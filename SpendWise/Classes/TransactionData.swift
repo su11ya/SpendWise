30e36df22a1486828c27a15bbe18543c35e98b99
Ya Su <suyawow@gmail.com>
2023-03-24 11:38:16 -0400

    Initial Commit

---
+++ SpendWise/Classes/TransactionData.swift
@@ -0,0 +1,32 @@
+//
+//  TransactionData.swift
+//  SpendWise
+//
+//  Created by ya su on 2023-03-24.
+//
+
+import UIKit
+
+class TransactionData: NSObject {
+    
+    var id : Int?
+    var date : String?
+    var category : String?
+    var amount : String?
+    var type : String?
+    var descriptions : String?
+    var balance : String?
+    
+    func initWithData(theRow i : Int, theDate d : String, theCategory c : String, theAmount a : String,
+                     theType t : String, theDescriptions des : String, theBalance b : String){
+        id = i
+        date = d
+        category = c
+        amount = a
+        type = t
+        descriptions = des
+        balance = b
+        
+    }
+
+}
---
+++ SpendWise/ViewControllers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SpendWise
+//
+//  Created by ya su on 2023-03-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+    }
+
+
+}
+
